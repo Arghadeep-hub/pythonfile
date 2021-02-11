7c0b4d95ce4078166738ca2c49c03377be5a329f
Arghadeep Mallick <66899260+Arghadeep-hub@users.noreply.github.com>
2021-02-11 22:34:47 +0530

    my python file

---
+++ tutorial.py
@@ -0,0 +1,23 @@
+def moveToEnd(arr, n):
+    count = 0
+    for i in range(0, n):
+        if (arr[i] != 0):
+            arr[count], arr[i] = arr[i], arr[count]
+            count += 1
+
+
+def printArray(arr, n):
+    for i in range(0, n):
+        print(arr[i], end=" ")
+
+
+arr = [0, 5, 9, 0, 4, 1]
+n = len(arr)
+
+print("original Array:", end=" ")
+printArray(arr, n)
+
+moveToEnd(arr, n)
+
+print("\nModified Array:", end=" ")
+printArray(arr, n)
